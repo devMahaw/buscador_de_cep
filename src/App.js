26c88880dd20620a2449d06fde155e87a7283f63
Marcelo Dantas <mahawprogramacao@gmail.com>
2024-02-18 13:11:57 -0300

    feat: app styles included

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { FiSearch } from "react-icons/fi";
+import "./styles.css";
+
 function App() {
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
-
-      <div className="containerInput">
-        <input type="text" placeholder="Digite seu cep" />
-        <button type="button" className="buttonSearch"></button>
-      </div>
+      <header className="containerInput">
+        <input type="text" placeholder="Digite seu CEP" />
+        <button type="button" className="buttonSearch">
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </header>
+      <main className="main">
+        <h2>CEP: 09209-302</h2>
+        <span>Rua: Rua Teste</span>
+        <span>Complemento: Casa</span>
+        <span>Bairro: Vila La ele</span>
+        <span>Cidade/Estado: São Paulo - SP</span>
+      </main>
     </div>
   );
 }
